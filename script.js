7b13e8ebc1b24f6c768e44672e597d2fe50ff634
christian <lan21011@byui.edu>
2025-01-18 20:50:29 -0700

    Update information, and added more room for extra projects

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+
+function toggleProjects() {
+const extraProjects = document.getElementById("extra-projects");
+const toggleButton = document.getElementById("toggle-projects");
+
+if (extraProjects.style.display === "none") {
+    extraProjects.style.display = "grid";
+    toggleButton.innerText = "Show Less";
+} else {
+    extraProjects.style.display = "none";
+    toggleButton.innerText = "Show All";
+}
+}
